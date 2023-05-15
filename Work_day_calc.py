f5ae558a73ad0e4085aff928ee82d632e9573e6f
Dredd85 <piotr.siergiej@gmail.com>
2023-05-15 11:12:42 +0200

    Added new file

---
+++ Work_day_calc.py
@@ -0,0 +1,40 @@
+import calendar 
+from datetime import date
+from calendar import monthrange
+
+today = date.today()
+month = today.month
+year = today.year
+
+working_days = list()
+
+weekday, number_of_days = calendar.monthrange(year, month)
+
+print('The first day of month is:', calendar.day_name[weekday])
+print('This month has:', number_of_days, 'days')
+
+cal = calendar.monthcalendar(year, month)
+for weeks in cal:
+    work_days = weeks[:5]
+    for days in work_days:
+        if days > 0:
+            working_days.append(days)
+
+print('This month has:', len(working_days), 'working days')
+print(calendar.month(year, month))
+
+add_days = int(input('How many addtitional work days did You have? '))
+sum_worked_days = len(working_days) + add_days
+
+print('You worked',sum_worked_days,'days')
+
+pay = int(input('What is your pay for hour? '))
+hours = int(input('How long is your working day? '))
+payment = sum_worked_days * pay * hours
+
+print('Your payment for this months is:', payment, 'zł')
+        
+
+
+
+
